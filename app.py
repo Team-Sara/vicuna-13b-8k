dfcf60f6aa1a17cf7eb1102e197bcb02907317ff
teamsaraadmin <137902736+teamsaraadmin@users.noreply.github.com>
2023-07-22 06:52:59 +0530

    Update app.py

--- app.py
+++ app.py
@@ -30,10 +30,9 @@ class InferlessPythonModel:
             model=self.model,
             tokenizer=self.tokenizer,
             max_new_tokens=512,
-            temperature=3.55,
+            temperature=0.7,
             top_p=0.14,
-            top_k=0.49,
-            repetition_penalty=1.20
+            repetition_penalty=1.17
         )
         generated_text = pipe(prompt_template)[0]['generated_text']
         return {"generated_text": generated_text}
